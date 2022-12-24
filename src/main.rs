ef6889ba1f7a482b16d34e3560dc70ff30c79c85
Aarush Gupta <git@aarushgupta.com>
2022-12-23 19:41:21 -0800

    woah its an idt! and fancy mod.rs magic

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #![no_std]
 #![no_main]
 
+#![feature(abi_x86_interrupt)]
+
+mod api;
+mod sys;
+use crate::sys::idt::{init_idt};
+
 use core::panic::PanicInfo;
 
-pub mod api;
+extern crate x86_64;
+use self::x86_64::instructions::interrupts;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!("[error] {}", info);
     loop {}
 }
 
 fn init() {
     println!("[info] loaded sanity");
+    println!("[info] loading idt...");
+    init_idt();
+    x86_64::instructions::interrupts::int3();
+    println!("[info] sanity init complete");
 }
 
 #[no_mangle]
---
+++ src/sys/idt.rs
@@ -0,0 +1,23 @@
+use crate::println;
+
+extern crate lazy_static;
+use self::lazy_static::lazy_static;
+
+extern crate x86_64;
+use self::x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
