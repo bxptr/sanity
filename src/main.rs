47276aaa889f185761fb6d43d2e47c997f81777c
Aarush Gupta <git@aarushgupta.com>
2022-12-27 13:23:32 -0800

    WOWZAH its paging AND a heap :O

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use sanity::{println, print};
 
 use core::panic::PanicInfo;
 use x86_64;
+use bootloader::{BootInfo, entry_point};
+
+extern crate alloc;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 
 fn hlt() -> ! {
     loop {
@@ -20,7 +24,7 @@ fn panic(info: &PanicInfo) -> ! {
     hlt();
 }
 
-fn init() {
+fn init(boot_info: &'static BootInfo) {
     println!("[info] loaded sanity");
     println!("[info] loading idt...");
     sys::idt::init();
@@ -28,13 +32,32 @@ fn init() {
     sys::gdt::init();
     println!("[info] loading pic...");
     sys::pic::init();
+    println!("[info] loading mem...");
+    sys::mem::init(boot_info);
     println!("[info] sanity init complete");
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    init();
+fn heap_test() {
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
+}
+
+pub fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    init(boot_info);
     println!("no crash :)");
+    heap_test();
     print!("> ");
     hlt();
 }
+
+entry_point!(kernel_main);
--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -2,3 +2,5 @@ pub mod idt;
 pub mod gdt;
 pub mod pic;
 pub mod keyboard;
+pub mod mem;
+pub mod allocator;
